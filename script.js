9860c93d1b5544388b649292c1eb8ae62d009c9d
DesislavaMetodieva <82023025+DesislavaMetodieva@users.noreply.github.com>
2023-01-08 10:13:07 +0200

    Added new comments for more clarification

--- script.js
+++ script.js
@@ -1,63 +1,3 @@
-// Should configure exit condition to say that the user should at least a select a character type. We should configure 4 prompts for every Character type
-
-//
-
-// * Generate a password when the button is clicked
-//   * Present a series of prompts for password criteria
- // Prompt: How long would you like your password to be (10 - 64)
-  // Validation: We need to make sure the number is numerical is it is within the 10 and 64 digits
-
-// * Length of password
-//   * At least 10 characters but no more than 64.
-
-// Get the initial input of the client
-
-// var passNum = (prompt("Please select the lenght of your new password (between 10 and 64 characters long)"));
-
-// // convert input to number
-
-// let passNumber = parseInt(passNum);
-
-// // Next step is to validate if input meets all requirements
-
-// if (!Number.isNaN(passNumber) && passNumber < 10 && passNumber > 64) {
-//   console.log("Thanks");  
-// } else {
-//   prompt ("Please choose another password lenght between 10 and 64 characters");
-// };
-
-
-
-
-// * Character types
-
-
-//   * Lowercase
-
-// Next confirm.prompt will be to ask the user whether they need Lowercase
-// If yes, we need to include the Lower cases
-
-//   * Uppercase
-
-// Next confirm.prompt will be to ask the user whether they need Upper cases
-// If yes, we need to include the Upper cases
-
-//   * Numeric
-
-// Next confirm.prompt will be to ask the user whether they need Numberic
-// If yes, we need to include the Numeric cases
-
-//   * Special characters ($@%&*, etc)
-
-// Next confirm.prompt will be to ask the user whether they need Special character
-// If yes, we need to include the Special character
-
-// minimum requirements to have is 1 character type and minimum lenght of password
-
-// * Code should validate for each input and at least one character type should be selected
-
-// * Once prompts are answered then the password should be generated and displayed in an alert or written to the page
-
 
 // Array of special characters to be included in password
 var specialCharacters = [
@@ -151,9 +91,7 @@ var upperCasedCharacters = [
 
 // Function for getting a random element from an array
 function getRandom(passChars, passNumber) {
-  
 var password = "";
-
   for (i = 0; i < passNumber; i++){
    password += passChars[Math.floor(Math.random() * passChars.length)]
  };
@@ -170,17 +108,20 @@ let generatePassword = function () {
 // list of chosen characters
 var passChars = [];
 
-// Get the initial input of the client
-
+// Get the initial input of the user
 var passNum = prompt("Please select the length of your new password (between 10 and 64 characters long)");
 
 // // convert input to number
 passNum = parseInt(passNum);
 
+// Input validation
 if (passNum < 10) {
   alert ("Password should be longer than 10 characters!");
   return "";
 };
+
+// checking if the user input is a number
+// console.log(typeof passNum);
 
 if (passNum > 64) {
   alert ("Password should have no more than 64 characters");
